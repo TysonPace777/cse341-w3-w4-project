a65e3e3b6dfaf0053d94d0adb2e8a2762a6dd5be
tysonpace777 <32.tpace@gmail.com>
2025-11-20 13:34:55 -0700

    added hashed passwords

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,13 @@
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
+import bcrypt from 'bcrypt';
+
+//hash passwords
+async function hashPassword(password) {
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+    return passwordHash;
+}
 
 //gets one user
 const getSingle = async (req, res) => {
@@ -22,13 +30,15 @@ const getSingle = async (req, res) => {
 // Adds a new user to the DB
 const createUser = async (req, res) => {
     try {
+        const hashedPassword = await hashPassword(req.body.password);
+
         const contact = {
             firstName: req.body.firstName,
             lastName: req.body.lastName,
             username: req.body.username,
             email: req.body.email,
             birthday: req.body.birthday,
-            password: req.body.password,
+            password: hashedPassword,
             membershipLevel: req.body.membershipLevel
         };
         const response = await mongodb.getDatabase().db().collection('users').insertOne(contact);
